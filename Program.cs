714a9acdd2fbc206008016fc1778f49cba08c4f3
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-10 01:55:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Private
+{
+     class Program
+    {
+        
+        private string Ime = "Milovan";
+        private string Prezime = "Jovanovic";
+        private int Br = 100;
+
+        public string Id()
+        {
+            return Ime + " " + Prezime;
+        }
+        public int Mnozi() 
+        {
+         return Br * 2;
+        }
+                
+     
+        static void Main(string[] args)
+        {
+           Program p1 = new Program();
+
+            Console.WriteLine(p1.Id());
+            Console.WriteLine(p1.Mnozi());
+
+        }
+    }
+}
